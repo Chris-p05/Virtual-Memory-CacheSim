7310ee439b87920368fc7e646a0e3d24ae595ca1
Chris-p05 <146773988+Chris-p05@users.noreply.github.com>
2025-10-24 22:00:52 -0500

    carlos' part
    added carlos' part with the physical memory calculations

--- FileTracer.py
+++ FileTracer.py
@@ -135,4 +135,100 @@ class FileTracer:
     def get_virtual_address_space(self):
         return self.__virtual_address_space
    
+<<<<<<< Updated upstream
     
+=======
+    #Christopher Peters Part starts here >:) 
+
+  # ---------- Cache Calculated Values ----------
+    
+    def get_cache_size_bytes(self):
+        return self.get_cache_size() * 1024
+    
+    def get_total_blocks(self):
+        return self.get_cache_size_bytes() // self.get_block_size()
+    
+    def get_total_rows(self):
+        return self.get_total_blocks() // self.get_associativity()
+    
+    def get_index_size(self):
+        return int(math.log2(self.get_total_rows()))
+    
+    def get_offset_size(self):
+        return int(math.log2(self.get_block_size()))
+    
+    def get_physical_memory_bytes(self):
+        return self.get_physical_memory() * 1024 * 1024
+    
+    def get_total_address_bits(self):
+        return int(math.log2(self.get_physical_memory_bytes()))
+    
+    def get_tag_size(self):
+        return self.get_total_address_bits() - self.get_index_size() - self.get_offset_size()
+    
+    def get_overhead_bits_per_block(self):
+        return 1 + self.get_tag_size()
+    
+    def get_total_overhead_bits(self):
+        return self.get_overhead_bits_per_block() * self.get_total_blocks()
+    
+    def get_overhead_bytes(self):
+        return math.ceil(self.get_total_overhead_bits() / 8)
+    
+    def get_implementation_memory_bytes(self):
+        return self.get_cache_size_bytes() + self.get_overhead_bytes()
+    
+    def get_implementation_memory_kb(self):
+        return self.get_implementation_memory_bytes() / 1024
+    
+    def get_cost(self):
+        return self.get_implementation_memory_kb() * 0.07
+    
+    # --------------- Print menu for Cache Calculated Values -----------------
+    
+    def print_cache_calculated_values(self):
+        """Print the Cache Calculated Values section"""
+        print("***** Cache Calculated Values *****")
+        print(f"Total # Blocks:                  {self.get_total_blocks()}")
+        print(f"Tag Size:                        {self.get_tag_size()} bits")
+        print(f"Index Size:                      {self.get_index_size()} bits")
+        print(f"Total # Rows:                    {self.get_total_rows()}")
+        print(f"Overhead Size:                   {self.get_overhead_bytes()} bytes")
+        print(f"Implementation Memory Size:      {self.get_implementation_memory_kb():.2f} KB ({self.get_implementation_memory_bytes()} bytes)")
+        print(f"Cost:                            ${self.get_cost():.2f} @ $0.07 per KB")
+        print()
+
+    #Carlos Mejia Rosales Part starts here 🙁
+
+  # ---------- Physical Memory Calculated Values ----------
+
+    def print_physical_memory_calculations(self):
+
+        """Calculates and prints the Physical Memory Calculated Values section."""
+
+        PAGE_SIZE_KB = 4
+        PTE_ENTRIES_PER_PROCESS = 524288
+
+        physical_memory_mb = self.get_physical_memory()
+        system_percent = self.get_physical_memory_os_usage() / 100.0
+        num_trace_files = len(self.get_trace_file())
+
+        total_memory_kb = physical_memory_mb * 1024
+        num_physical_pages = total_memory_kb // PAGE_SIZE_KB
+
+        num_pages_for_system = int(num_physical_pages * system_percent)
+
+        PPN_BITS = 18 
+        VALID_BIT = 1
+        size_of_pte_bits = PPN_BITS + VALID_BIT
+
+        total_bits = PTE_ENTRIES_PER_PROCESS * num_trace_files * size_of_pte_bits
+        total_ram_bytes = total_bits // 8
+ 
+
+        print("\n *****Physical Memory Calculated Values*****")
+        print(f"\nNumber of Physical Pages: {num_physical_pages:>20}")
+        print(f"Number of Pages for System: {num_pages_for_system:>18} ")
+        print(f"Size of Page Table Entry: {size_of_pte_bits:>20} bits ")
+        print(f"Total RAM for Page Table(s): {total_ram_bytes:>15} bytes")
+>>>>>>> Stashed changes
--- VMCacheSim.py
+++ VMCacheSim.py
@@ -5,7 +5,13 @@ def main():
         print("\nCache Simulator - CS 3853 - Team #7\n")
         tracer = FileTracer()
         tracer.load()
+<<<<<<< Updated upstream
         tracer.print_config()        
+=======
+        tracer.print_config() 
+        tracer.print_cache_calculated_values()      
+        tracer.print_physical_memory_calculations()
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
         main()
