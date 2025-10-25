dd2028ce876a39c321c9aa9fb9c65fe653570a18
MKorolyova <mkorolyova52@gmail.com>
2025-10-25 00:09:37 -0500

    Added FileTracer class for parsing trace files and implemented simulation_milestone_1 function to generate and save simulation output

--- VMCacheSim.py
+++ VMCacheSim.py
@@ -1,13 +1,17 @@
-from FileTracer import FileTracer
+from linecache import cache
+from CacheValuesCalculator import CacheValuesCalculator
 
+def simulation_milestone_1():
+        cache_values_calculator = CacheValuesCalculator()
+        cache_values_calculator.load()
+        config_str = cache_values_calculator.get_program_output()
+        print("Enter the simulation number: ")
+        simulation_number = input()
+        with open(f"Team_07_Sim_{simulation_number}_M#1.txt", "w") as file:
+                file.write(config_str)
 
 def main():
-        print("\nCache Simulator - CS 3853 - Team #7\n")
-        tracer = FileTracer()
-        tracer.load()
-        tracer.print_config() 
-        tracer.print_cache_calculated_values()   
-        tracer.print_physical_memory_calculations()   
+        simulation_milestone_1()
 
 if __name__ == "__main__":
         main()
